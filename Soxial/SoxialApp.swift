cbaef1d2cb6b0929f00c46cf31dae298c90889fc
Jayson <soknang_chheng@yahoo.com>
2021-03-09 11:58:02 -0600

    Initial Commit

---
+++ Soxial/SoxialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoxialApp.swift
+//  Soxial
+//
+//  Created by Jayson Wavy  on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SoxialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
